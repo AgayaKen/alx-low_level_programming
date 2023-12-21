8366aea235fd5b1ad1471a59f1a649abf4429ee6
AgayaKen <kennedyagaya@gmail.com>
2023-12-21 12:21:15 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+/**
+ * _isupper - Checking caracter
+ * check char: Character for checking
+ *
+ * Return: 1 if the character is uppercase, 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
