eaef850aa8b45720d4433e6383cd6b17466652d7
AgayaKen <kennedyagaya@gmail.com>
2024-01-16 05:36:45 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * _islower - Test wheter c is lowercase
+ * @c: Caracter to test
+ *
+ * Return: 1 if c is lowercase, 0 otherwise.
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
