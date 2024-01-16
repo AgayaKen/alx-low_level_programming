091cdb2865f99208a4eba72c4206aab1543bfb9a
AgayaKen <kennedyagaya@gmail.com>
2024-01-16 05:42:58 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * _isalpha - Test wheter c is lowercase
+ * @c: Caracter to test
+ *
+ * Return: 1 if c character is in the alphabet, 0 otherwise.
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
