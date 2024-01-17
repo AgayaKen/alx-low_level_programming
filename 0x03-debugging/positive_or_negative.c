70955ae94de8d8e548e36f19e431aac6834a4221
AgayaKen <kennedyagaya@gmail.com>
2024-01-17 02:22:37 -0800

    pos_neg

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int n = 0;
+
+    printf("Testing positive_or_negative(%d):\n", n);
+    positive_or_negative(n);
+    printf("Expected output: 0 is zero\n");
+
+    return (0);
+}
+
