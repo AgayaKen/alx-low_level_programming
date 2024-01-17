3717af1d31c9b91ac52e762cf75c08061d1c561d
AgayaKen <kennedyagaya@gmail.com>
2024-01-17 02:33:20 -0800

    pos_neg

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -5,14 +5,15 @@
  *@positive_or_negative(n): Test if a given input is negative or positive
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    int n = 0;
+int n = 0;
 
-    printf("Testing positive_or_negative(%d):\n", n);
-    positive_or_negative(n);
-    printf("Expected output: 0 is zero\n");
+printf("Testing positive_or_negative(%d):\n", n);
+positive_or_negative(n);
+printf("Expected output: 0 is zero\n");
 
-    return (0);
+return (0);
 }
 
